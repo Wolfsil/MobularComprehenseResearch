16968a180645611b72b92c7577b3b287c7208acc
whiterabit <37937078+Wolfsil@users.noreply.github.com>
2021-04-01 18:31:26 +0900

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -2,43 +2,45 @@
 using namespace std;
 
 
-int get_remainder(int solution, int modular) {
-	while (solution < 0) { solution = modular + solution; }
-	return solution % modular;
+int get_remainder(int solution, int modulo) {
+	if (solution < 0){ 
+		return solution % modulo + modulo;
+	}
+	return solution % modulo;
 }
 
-int get_inverse(int value, int modular) {
+int get_inverse(int value, int modulo) {
 	int result=1;
-	value =get_remainder(value, modular);
-	for (int i = 0; i < modular - 2; i++){ result = (result * value) % modular;}
+	for (int i = 0; i < modulo - 2; i++){ 
+		result = (result * value) % modulo;
+	}
 	return result;
 }
 
 
-int modular_solution(int remainder[],int modular[],int size) {
+int garner_solution(int remainder[], int modulo[], int size) {
 
-	int cross_modural = modular[0];
-	int modular_cross = 1; 
+	int cross_modulo = modulo[0];
+	int modulo_cross = 1;
 	int result = remainder[0];
 	for (int i = 1; i < size; i++) {
-		modular_cross = modular_cross * modular[i - 1];
-		remainder[i] = get_remainder((remainder[i] - result), modular[i])
-			*get_inverse(modular_cross,modular[i])%modular[i];
-		result = (result + remainder[i] * modular_cross);
+		modulo_cross = modulo_cross * modulo[i - 1];
+		remainder[i] = get_remainder( (remainder[i] - result) * get_inverse(modulo_cross, modulo[i]), modulo[i]);
+		result = (result + remainder[i] * modulo_cross);
 	}
-	printf("단 하나의 해는: %d", result);
-	return 0;
+	return result;
 }
 
-
 int main() {
-	int solution = 57;
 	/*int re[4] = {1,2,3,4};
 	int mo[4] = { 2,3,5,7 };*/
-	/*int re[3] = {2,3,2};
-	int mo[3] = {3,5,7};*/
-	int re[5] = { 0,0,0,0 };
-	int mo[5] = { 2,3,5,7 };
-	modular_solution(re, mo ,sizeof(mo)/sizeof(typeid(mo).name()));
+	int re[3] = {2,3,2};
+	int mo[3] = {3,5,7};
+	//int re[8] = { 0,2,4,6,13,12,22,30};
+	//int mo[8] = { 2,3,5,7,17,19,23,31};
+	int result=garner_solution(re, mo, sizeof(mo) / sizeof(int));
+	
+	//검사
+	cout << "결과: "<< result << " " << result%mo[1] << endl;
 	return 0;
 }
